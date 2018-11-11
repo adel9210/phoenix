a6a6b156eca036dd1749d5ca24a92763152143f6
adel <adelabodoma@gmail.com>
2018-11-11 17:28:44 +0200

    add page loader

--- js/myjs.js
+++ js/myjs.js
@@ -82,3 +82,9 @@ window.onscroll = function () {
 	scrollFunction();
 };
 
+//paste this code under the head tag or in a separate js file.
+	// Wait for window load
+	$(window).on('load',function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	});
