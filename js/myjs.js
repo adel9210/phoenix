508df111fdc4509bba4db5968ead583d147c0793
adel <adelabodoma@gmail.com>
2018-11-23 00:56:42 +0200

    Fix Transition

--- js/myjs.js
+++ js/myjs.js
@@ -82,9 +82,3 @@ window.onscroll = function () {
 	scrollFunction();
 };
 
-//paste this code under the head tag or in a separate js file.
-	// Wait for window load
-	$(window).on('load',function() {
-		// Animate loader off screen
-		$(".se-pre-con").fadeOut("slow");;
-	});
